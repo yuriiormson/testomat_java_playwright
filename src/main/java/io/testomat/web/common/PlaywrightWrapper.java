424dc139c9e95ace36089fe59f575a8356fe3e54
Yurii Ormson <seraph2401@gmail.com>
2023-11-17 07:26:35 +0200

    add DOMCONTENTLOADED to find

--- src/main/java/io/testomat/web/common/PlaywrightWrapper.java
+++ src/main/java/io/testomat/web/common/PlaywrightWrapper.java
@@ -90,27 +90,27 @@ public class PlaywrightWrapper {
 
 
     public static LocatorActions $(String selector) {
-        pw().getPage().waitForLoadState(LoadState.NETWORKIDLE);
+        pw().getPage().waitForLoadState(LoadState.DOMCONTENTLOADED);
         return find(selector);
     }
 
     public static LocatorActions $(String selector, String text) {
-        pw().getPage().waitForLoadState(LoadState.NETWORKIDLE);
+        pw().getPage().waitForLoadState(LoadState.DOMCONTENTLOADED);
         return find(selector,text);
     }
 
     public static LocatorActions find(String selector) {
-        pw().getPage().waitForLoadState(LoadState.NETWORKIDLE);
+        pw().getPage().waitForLoadState(LoadState.DOMCONTENTLOADED);
         return new LocatorActions(pw().getPage().locator(selector).first());
     }
 
     public static LocatorActions findAll(String selector) {
-        pw().getPage().waitForLoadState(LoadState.NETWORKIDLE);
+        pw().getPage().waitForLoadState(LoadState.DOMCONTENTLOADED);
         return new LocatorActions(pw().getPage().locator(selector));
     }
 
     public static LocatorActions find(String selector, String text) {
-        pw().getPage().waitForLoadState(LoadState.NETWORKIDLE);
+        pw().getPage().waitForLoadState(LoadState.DOMCONTENTLOADED);
         return new LocatorActions(pw().getPage().locator(selector).filter(
                 new Locator.FilterOptions().setHasText(text)
         ));
